6f9847b4a1fb941c5b09da1e8015dc1f6e549697
Kim Kakiiza <kimkakiiza@gmail.com>
2025-12-22 23:06:08 -0500

    Migration to new Supabase

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -7,3 +7,6 @@ export const supabaseBrowser = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
